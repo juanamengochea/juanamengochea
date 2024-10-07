e5d659eb7410369616e5a22ef4de91415d16dc8d
juanamengochea <juamengo05@gmail.com>
2024-10-07 18:34:05 -0300

    agregando archivo vehiculos

---
+++ RegistroAutomotor/Vehiculo.ts
@@ -0,0 +1,37 @@
+export class Vehiculo {
+    private marca: string;
+    private modelo: string;
+    private tipo: string;
+    private año: number;
+    private añofabricacion: number;
+    private color: string;
+    
+   constructor(marca: string, modelo: string, año: number, añofabricacion: number, color?: string) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.añofabricacion = añofabricacion;
+        this.color = color || "violeta"; 
+       
+    }
+getVehiculo(): string {
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Año: ${this.año}, Año de Fabricación: ${this.añofabricacion}, Color: ${this.color}`;
+    }
+setModificarVehiculo(marcanueva?: string, modelonuevo?: string, añonuevo?: number, nuevoañofabricacion?: number, colornuevo?: string): void {
+        if (marcanueva) {
+            this.marca = marcanueva;
+        }
+        if (modelonuevo) {
+            this.modelo = modelonuevo;
+        }
+        if (añonuevo) {
+            this.año = añonuevo;
+        }
+        if (nuevoañofabricacion) {
+            this.añofabricacion = nuevoañofabricacion;
+        }
+        if (colornuevo) {
+            this.color = colornuevo;
+        }
+    }
+}  
